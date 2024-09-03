1cf1c020d69443362712f3ca025fe2931aae1991
Fahad <fahadshahbaz166@gmail.com>
2024-09-03 18:14:12 +0500

    Project setup with Context api

---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,25 @@
+import { createContext, useContext } from "react";
+
+const TodoContext = createContext({
+    // First it will take default values always
+    // This todo is serving as property, which is array and has objects
+    todos: [
+        {
+            id: 1,
+            todo: "Todo msg",
+            completed: false
+        }
+    ],
+    addTodo: (todo)=>{},
+    updateTodo: (id, todo)=>{},
+    deleteTodo: (id)=>{},
+    toggleComplete: (id)=>{}
+});
+
+export const useTodo = () => {
+ // It will return object of TodoContext
+  return useContext(TodoContext);
+};
+
+// We export provider from here to avoid wrapping main.jsx with provider and passing TodoContext as prop
+export const Todoprovider = TodoContext.Provider;
